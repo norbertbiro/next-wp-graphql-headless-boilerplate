d828ae7a94f136744cbe895b589e9d2cd13980ce
norbertbiro <norbert.biro@toptal.com>
2020-10-29 08:24:18 +0200

    # removed plugins

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wpgraphql' );
+define( 'DB_NAME', 'boilerplate' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
